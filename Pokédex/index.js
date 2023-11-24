2e0f2955f3b772a80d9282ed1dba7871d57f0bf8
Gabriel <gabrielhepper403@gmail.com>
2023-11-23 21:46:01 -0300

    Lista quase pronta

---
+++ Pokédex/index.js
@@ -0,0 +1,49 @@
+fetch(`https://pokeapi.co/api/v2/pokemon`)
+  .then(response => response.json())
+  .then(function(data) {
+    let lista = document.getElementById("List")
+
+    data.results.forEach(function(pokemon) {
+      fetch(pokemon.url)
+        .then(response => response.json())
+        .then(function(pokemonData) {
+          const pokemons = document.createElement("li");
+
+          const pokemonType = pokemonData.types.length > 0 ? pokemonData.types[0].type.name : '';
+
+          pokemons.classList.add(`type-${pokemonType}`);
+
+          let buttonDiv = document.createElement("div");
+          buttonDiv.classList.add("buttonDiv");
+
+          let detalhesButton = document.createElement("button");
+          detalhesButton.innerText = "Detalhes";
+          detalhesButton.classList.add("detalhesButton");
+
+          let adicionarButton = document.createElement("button");
+          adicionarButton.innerText = "Adicionar";
+          adicionarButton.classList.add("adicionarButton");
+
+          detalhesButton.onclick = function() {
+            console.log("Detalhes do Pokémon:", pokemonData.name);
+          };
+
+          adicionarButton.onclick = function() {
+            console.log("Pokémon adicionado com sucesso!");
+          };
+
+          buttonDiv.appendChild(detalhesButton);
+          buttonDiv.appendChild(adicionarButton);
+
+          pokemons.innerHTML = `
+            <img src="${pokemonData.sprites.front_default}" alt="Foto do Pokémon">
+            <div>
+                <p>${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</p>
+            </div>
+          `;
+
+          pokemons.appendChild(buttonDiv);
+          lista.appendChild(pokemons);
+        })
+    })
+  });
