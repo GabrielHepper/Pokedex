f658408ac7c880e94da327a78f9d7c73eeec6575
Gabriel <gabrielhepper403@gmail.com>
2023-12-04 20:22:45 -0300

    tt

--- Pokédex/index.js
+++ Pokédex/index.js
@@ -12,7 +12,6 @@ fetch(`https://pokeapi.co/api/v2/pokemon`)
           const pokemonType = pokemonData.types.length > 0 ? pokemonData.types[0].type.name : '';
           pokemons.classList.add(`type-${pokemonType}`);
 
-         
           pokemons.id =`pokemon${pokemonData.id}`
 
           pokemons.innerHTML = `
@@ -30,7 +29,7 @@ fetch(`https://pokeapi.co/api/v2/pokemon`)
 
               <div class="buttonDiv">
                 <button class="adicionarButton" onClick="adiciona(${pokemonData.id})">Adicionar</button>
-                <button class="detalhesButton">Detalhes</button>
+                <button class="detalhesButton" onClick="abrirDialog(${pokemonData.id})">Detalhes</button>
               </div>
             </div>
           `;
@@ -43,6 +42,8 @@ fetch(`https://pokeapi.co/api/v2/pokemon`)
                 pokemons.style.color = "red";
             } else if (type === "water") {
                 pokemons.style.color = "rgb(80, 80, 270)";
+            } else if (type === "poison"){
+                pokemons.style.color = "purple";
             }
           }
 
@@ -50,6 +51,73 @@ fetch(`https://pokeapi.co/api/v2/pokemon`)
         })
     })
   });
+
+  function abrirDialog(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then(response => response.json())
+      .then(function(pokemonData) {
+        const dialog = document.createElement('dialog');
+        dialog.classList.add("dialog");
+  
+        const abilities = pokemonData.abilities.map(ability => ability.ability.name);
+  
+        const abilityPromises = abilities.map(abilityName =>
+          fetch(`https://pokeapi.co/api/v2/ability/${abilityName}`)
+            .then(response => response.json())
+        );
+  
+        Promise.all(abilityPromises)
+          .then(abilityDataArray => {
+            const abilitiesInfo = abilityDataArray.map((abilityData, index) => {
+              const effectEntry = abilityData.effect_entries.find(entry => entry.language.name === 'en');
+              const power = effectEntry?.effect ? 1 : 0; // Verifica se há algum efeito
+  
+              const barWidth = Math.min(power * 100, 100);
+  
+              return `
+                <div class="status">
+                  <p>${abilities[index]}</p>
+                  <div class="barra-dano-container">
+                    <div class="barra-dano" style="width: ${barWidth}%"></div>
+                    <div class="numero-dano">${power}</div>
+                  </div>
+                </div>
+              `;
+            });
+  
+            dialog.innerHTML = `
+              <div class="nomeDialog">
+                <h2>${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h2>
+              </div>
+              ${abilitiesInfo.join('')}
+            `;
+  
+            document.body.appendChild(dialog);
+  
+            dialog.addEventListener('click', function(event) {
+              if (event.target === dialog) {
+                dialog.close();
+              }
+            });
+  
+            dialog.showModal();
+          });
+      });
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
   function adiciona(id) {
     const div = document.getElementById(`pokemon${id}`)
